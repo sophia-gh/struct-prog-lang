2e141430d48c62e94f25e01a9756f2cb1f199d9f
Greg DeLozier <gregdelozier@users.noreply.github.com>
2024-08-26 21:11:35 +0000

    initial tokenizer

---
+++ topic-01-simple-expressions/tokenizer.py
@@ -0,0 +1,71 @@
+# tokenizer
+
+""" 
+break character stream into tokens, provide a token stream 
+"""
+
+import re
+
+patterns = [
+    ["\\(", "("],
+    ["\\)", ")"],
+    ["\\+", "+"],
+    ["\\-", "-"],
+    ["\\*", "*"],
+    ["\\/", "/"],
+    ["(\\d+\\.\\d*)|(\\d*\.\\d+)|(\\d+)", "number"],
+]
+
+for pattern in patterns:
+    pattern[0] = re.compile(pattern[0])
+
+
+def tokenize(characters):
+    tokens = []
+    position = 0
+    while position < len(characters):
+        for pattern, tag in patterns:
+            match = pattern.match(characters, position)
+            if match:
+                break
+        assert match
+        token = {
+            "tag": tag,
+            "value": match.group(0),
+            "position": position,
+        }
+        tokens.append(token)
+        position = match.end()
+    for token in tokens:
+        if token["tag"] == "number":
+            if "." in token["value"]:
+                token["value"] = float(token["value"])
+            else:
+                token["value"] = int(token["value"])
+    return tokens
+
+
+def test_simple_tokens():
+    print("testing simple tokens")
+    assert tokenize("+") == [{"tag": "+", "value": "+", "position": 0}]
+    assert tokenize("-") == [{"tag": "-", "value": "-", "position": 0}]
+    i = 0
+    for char in "+-*/()":
+        tokens = tokenize(char)
+        assert tokens[0]["tag"] == char
+        assert tokens[0]["value"] == char
+        assert tokens[0]["position"] == i
+    for characters in ["+","-"]:
+        tokens = tokenize(characters)
+        assert tokens[0]["tag"] == characters
+        assert tokens[0]["value"] == characters
+    for number in ["123.45","1.", ".1", "123"]:
+        tokens = tokenize(number)
+        assert tokens[0]["tag"] == "number"
+        assert tokens[0]["value"] == float(number)
+
+if __name__ == "__main__":
+    test_simple_tokens()
+    # tokens = tokenize("123.45*+1234*/123*()***34235****")
+    # print(tokens)
+    print("done.")
