46fed379573ede9c164e431d2f103a5677410931
Sophia Testa <148911997+sophia-gh@users.noreply.github.com>
2024-09-11 18:51:15 +0000

    made some chnages to to parse term and parse expression functions to fix error and added test parse term

--- stesta4/stParser.py
+++ stesta4/stParser.py
@@ -25,13 +25,77 @@ def parse_simple_expression(tokens):
     if tokens[0]["tag"] == "-":
         node, tokens = parse_simple_expression(tokens[1:])
         node = {"tag":"negate", "value":node}
-        return node, tokens[1:]
+        return node, tokens
 
 def parse_expression(tokens):
     return parse_simple_expression(tokens), tokens[1:]
 
 def parse_factor(tokens):
     return parse_simple_expression(tokens)
+
+def parse_term(tokens):
+    """
+    term = factor { "*"|"/" factor }
+    """
+    node, tokens = parse_factor(tokens)
+    while tokens[0]["tag"] in ["*", "/"]:
+        tag = tokens[0]["tag"]
+        right_node, tokens = parse_factor(tokens[1:])
+        node = {"tag": tag, "left": node, "right": right_node}
+        if not tokens:
+            break  #if tokens is empty then at end of expression 
+    return node, tokens
+
+def parse_expression(tokens):
+    """
+    expression = term { "+"|"-" term }
+    """
+    node, tokens = parse_term(tokens)
+    while tokens[0]["tag"] in ["+", "-"]:
+        tag = tokens[0]["tag"]
+        right_node, tokens = parse_term(tokens[1:])
+        node = {"tag": tag, "left": node, "right": right_node}
+        if not tokens:
+            break  #if tokens is empty then at end of expression
+    return node, tokens
+
+#test functions ---------------------------------------------------------------------------------------------------
+def test_parse_expression():
+    """
+    expression = term { "+"|"-" term }
+    """
+    pass
+
+def test_parse_term():
+    """
+    term = factor { "*"|"/" factor }
+    """
+    print("testing parse term")
+    node, tokens = parse_term(tokenize("7*2"))
+    assert node == {
+        'tag': '*', 
+        'left': {'tag': 'number', 'value': 7, 'position': 0}, 
+        'right': {'tag': 'number', 'value': 2, 'position': 2}
+    }
+    node, tokens = parse_term(tokenize("8*3*7"))
+    assert node == {
+        'tag': '*', 
+        'left': {'tag': '*', 
+                 'left': {'tag': 'number', 'value': 8, 'position': 0}, 
+                 'right': {'tag': 'number', 'value': 3, 'position': 2}}, 
+        'right': {'tag': 'number', 'value': 7, 'position': 4}
+    }
+    print("done.")
+    
+    
+def test_parse_factor():
+    """
+    factor = simple_expression
+    """
+    print("testing factors")
+    for s in ["2", "(2)", "-2"]:
+        assert parse_factor(tokenize(s)) == parse_simple_expression(tokenize(s))
+    print("done")
 
 def test_parse_simple_expression():
     """
@@ -57,16 +121,12 @@ def test_parse_simple_expression():
         'tag': 'negate', 'value': {'tag': 'number', 'value': 2, 'position': 2}
     }
     print("done.")
-    
-def test_parse_factor():
-    """
-    factor = simple_expression
-    """
-    print("testing factors")
-    for s in ["2", "(2)", "-2"]:
-        assert parse_factor(tokenize(s)) == parse_simple_expression(tokenize(s))
-    print("done")
 
 if __name__ == "__main__":
     test_parse_simple_expression()
     test_parse_factor()
+    test_parse_term()
+    
+    
+
+#tokens is never populated by the parse_simple_expression() function
