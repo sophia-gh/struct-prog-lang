f3c9225ec21a214f64f39902565f4c953a46f926
Sophia Testa <148911997+sophia-gh@users.noreply.github.com>
2024-10-23 21:00:18 +0000

    functional code

---
+++ stesta4/functional.py
@@ -0,0 +1,67 @@
+# in lisp there was a single language instruction called CAR(contents of .. register)
+# ex (CAR '(1 2 3)) --> 1 returns first element of a list                "first"
+# (CDR '(1 2 3)) --> '(2 3) returns list of elements after first element "tail"
+
+def length(t):
+    if t == [ ]:
+        return 0
+    return 1 + length(tail(t))
+
+def first(t):
+    if t == [ ]:
+        return None
+    return t[0]
+
+def tail(t):
+    if t == []:
+        return []
+    return t[1:]
+
+def construct(n, t):
+    return [n] + t
+
+def concat(t, v):
+    if length(t) == 0:
+        return v
+    else: 
+        return construct(first(t), concat(tail(t), v))
+
+# tests ---------------------------------------------------------------------------------------------
+def test_length():
+    print("testing length()")
+    assert length([]) == 0
+    assert length([1]) == 1
+    assert length([1, 2, 3]) == 3
+
+def test_first():
+    print("testing first()")
+    assert first([]) == None
+    assert first([1]) == 1
+    assert first([1, 2, 3]) == 1
+
+def test_tail():
+    print("testing tail()")
+    assert tail([]) == []
+    assert tail([1]) == []
+    assert tail([1, 2, 3]) == [2, 3]
+
+def test_construct():
+    print("testing construct()")
+    assert construct(1, []) == [1]
+    assert construct(1, [2]) == [1, 2]
+    assert construct(1, [2, 3]) == [1, 2, 3]
+
+def test_concat():
+    print("testing concat()")
+    assert concat(1, []) == [1]
+    assert concat([1], [2]) == [1, 2]
+    assert concat([1,2], [3, 4]) == [1, 2, 3, 4]
+
+
+if __name__ == "__main__":
+    test_first()
+    test_tail()
+    test_construct()
+    test_construct()
+    print("done!!")
+    
